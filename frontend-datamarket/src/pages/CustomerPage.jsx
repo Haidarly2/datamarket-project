7d5a239698afce927d74d3643a139875f9f066c4
Haidarly2 <haidar.aly007@gmail.com>
2025-05-01 23:46:14 +0700

    commit add customer page

--- frontend-datamarket/src/pages/CustomerPage.jsx
+++ frontend-datamarket/src/pages/CustomerPage.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import { Box, Container } from "@mui/material";
+import HeroSection from '../components/HeroSection';
+import ProviderList from '../components/ProviderList';
+import PopularPackages from '../components/PopularPackages';
+import NavbarCustomers from "../components/NavbarCustomers";
+
 const CustomerPage = () => {
-    return <h1>Customer Page</h1>;
+    return (
+        <Box bgcolor="#EFF3FA">
+            <NavbarCustomers />
+            <Container maxWidth="lg" sx={{ mt: 4 }}>
+                <HeroSection />
+                <ProviderList />
+                <PopularPackages />
+            </Container>
+        </Box>
+    );
 };
 
 export default CustomerPage;
