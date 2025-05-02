daa9682bcd65d4369ce883d2ab9d56df3f7c19c6
Haidarly2 <haidar.aly007@gmail.com>
2025-05-02 19:24:37 +0700

    commit update context for app

--- frontend-datamarket/src/index.js
+++ frontend-datamarket/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+import { CheckoutProvider } from './context/CheckoutContext';
+import { PaymentProvider } from './context/PaymentContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CheckoutProvider>
+      <PaymentProvider>
+          <App />
+      </PaymentProvider>
+    </CheckoutProvider>
   </React.StrictMode>
 );
 
