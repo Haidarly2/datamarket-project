8df05a29a54636ec82023b99c3fe528f61b70072
Haidarly2 <haidar.aly007@gmail.com>
2025-05-03 08:41:29 +0700

    commit update api

--- frontend-datamarket/src/services/api.js
+++ frontend-datamarket/src/services/api.js
@@ -4,7 +4,17 @@ const api = axios.create({
   baseURL: 'http://localhost:3001', // atau sesuaikan port json-server kamu
 });
 
+// Fungsi untuk mengambil semua paket
 export const getPackages = () => api.get('/packages');
+
+// Fungsi untuk mengambil paket berdasarkan ID
+export const getPackageById = (id) => api.get(`/packages/${id}`);
+
+// Fungsi untuk mengambil data providers
 export const getProviders = () => api.get('/providers');
+
+// Fungsi untuk mengambil data add-ons
 export const getAddOns = () => api.get('/addons');
+
+// Fungsi untuk mengambil metode pembayaran
 export const getPaymentMethods = () => api.get('/paymentMethods');
